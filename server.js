a8a104f86b50c00d0b8293689eef714b3660ac3e
kr1sp1n <krispinone@googlemail.com>
2015-11-27 00:26:39 +0100

    fix issue with response format without callbacks

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var uuid = require('uuid')
 var _ = require('lodash')
 var Handlebars = require('handlebars')
 
+// json helper
+Handlebars.registerHelper('json', function (obj) {
+  if (obj === undefined) return '{}'
+  if (typeof obj === 'string') obj = JSON.parse(obj)
+  var str = JSON.stringify(obj)
+  return new Handlebars.SafeString(str)
+})
+
 var config = require(__dirname + '/config.js')
 var app = express()
 var port = config.port || 3030
@@ -18,6 +26,7 @@ var responses = {}
 app.use(bodyParser.json())
 
 var getCallbackOptionsByStatusCode = function (statusCode, callbacks) {
+  debug('RESPONSE', statusCode)
   var defaults = { method: 'GET', json: true }
   var opts = _.assign(defaults, callbacks[statusCode])
   // Check failure
@@ -33,6 +42,19 @@ var getCallbackOptionsByStatusCode = function (statusCode, callbacks) {
   return opts
 }
 
+var formatResponse = function (response_template, data) {
+  if (_.isObject(response_template)) {
+    response_template = JSON.stringify(response_template)
+  }
+  var template = Handlebars.compile(response_template)
+  var result = template(data)
+  try {
+    return JSON.parse(result)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 app.post('/', function (req, res) {
   var resource_type = 'http_request'
   var id = uuid.v1()
@@ -43,10 +65,17 @@ app.post('/', function (req, res) {
     href: [ endpoint, resource_type, id ].join('/')
   }
   requests[id] = item
-  debug('New request %s %s', opts.method, opts.uri)
+  debug('New request %s %s ...', opts.method, opts.uri)
+  debug(opts.headers)
   res.send(item)
+
+  if (req.body.response) {
+    opts.body = formatResponse(req.body.response, opts.body)
+  }
+
   request(opts, function (err, response, body) {
-    debug('...done')
+    debug('... done')
+    // debug(body)
     if (err) {
       debug(err)
       return
@@ -57,22 +86,15 @@ app.post('/', function (req, res) {
       var callback_opts = getCallbackOptionsByStatusCode(statusCode, callbacks)
       callback_opts.body = response
       if (callback_opts.response) {
-        if (_.isObject(callback_opts.response)) {
-          callback_opts.response = JSON.stringify(callback_opts.response)
-        }
-        var template = Handlebars.compile(callback_opts.response)
-        var result = template(response)
-        try {
-          callback_opts.body = JSON.parse(result)
-        } catch (e) {
-          console.log(e)
-        }
+        callback_opts.body = formatResponse(callback_opts.response, callback_opts.body)
       }
       responses[id] = response
       debug('Handle callbacks')
       request(callback_opts, function (err, response, body) {
         if (err) debug(err)
       })
+    } else {
+      debug('No callbacks!')
     }
   })
 })
@@ -92,6 +114,6 @@ if (!module.parent) {
   var server = app.listen(port, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log('Example app listening at http://%s:%s', host, port)
+    debug('listening at http://%s:%s', host, port)
   })
 }
